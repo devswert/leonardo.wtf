86eb461a2e01f70cdf02546b2c9a1216add1a8e8
Leonardo David <leo.david.mm@gmail.com>
2023-09-24 12:05:35 -0300

    Basic setup of Nuxt + TailwindCSS

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="bg-yellow-50 h-screen">
+        <div class="container mx-auto">
+            <NuxtLayout>
+                <NuxtPage></NuxtPage>
+            </NuxtLayout>
+        </div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1 class="text-yellow-900 text-3xl text-center font-mono">TailwindCSS Installed!</h1>
+    </div>
+</template>
